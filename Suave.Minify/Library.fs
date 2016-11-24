649ebaf20246de19cf2f9c9ef0ff940e966bb70d
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-11-24 11:50:47 -0500

    actually file order is important

--- Suave.Minify/Library.fs
+++ Suave.Minify/Library.fs
@@ -12,7 +12,7 @@ open Yahoo.Yui.Compressor
 let private jsCompressor  = JavaScriptCompressor()
 let private cssCompressor = CssCompressor()
 
-let mimify path filter =
+let mimify files filter =
   let output = new StringBuilder()
 
   let compressor =
@@ -23,7 +23,7 @@ let mimify path filter =
     else
       failwithf "Unsupported filter %s, only *.js and *.css are supported." filter
   
-  for file in Directory.EnumerateFiles(path,filter) do
+  for file in files do
     try
       let txt = File.ReadAllText(file)
       if file.Contains(".min.") then
@@ -39,25 +39,27 @@ let mimify path filter =
   
   output.ToString()
 
-let bundle path filter : WebPart =
+let bundlePart files filter : WebPart =
   context(fun ctx ->
-    let realpath = Files.resolvePath ctx.runtime.homeDirectory path
     match ctx.request.header "if-modified-since" with
     | Choice1Of2 v ->
       match Utils.Parse.dateTime v with
       | Choice1Of2 date ->
-        let files = Seq.map (fun x -> new FileInfo(x)) (Directory.EnumerateFiles(realpath,filter))
-        let lastModified = Seq.max (Seq.map (fun (x:FileInfo) -> x.LastWriteTimeUtc) files)
+        let lastModified = Seq.max (Seq.map (fun (x:FileInfo) -> x.LastWriteTimeUtc) (Seq.map (fun x -> new FileInfo(x)) files))
         if lastModified > date then
-          let output = mimify realpath filter
+          let output = mimify files filter
           Successful.OK output
         else
           Redirection.NOT_MODIFIED
       | Choice2Of2 parse_error -> RequestErrors.bad_request [||]
     | Choice2Of2 _ ->
-      let output = mimify realpath filter
+      let output = mimify files filter
       Successful.OK output)
 
+let bundle files filter =
+  context(fun ctx ->
+    bundlePart (Seq.map (fun x -> Files.resolvePath ctx.runtime.homeDirectory x) files) filter
+  )
 
 let jsBundle path : WebPart =
   Writers.setMimeType "application/x-javascript" >=> bundle path "*.js"
